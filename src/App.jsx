1376c688eb683b0ad8a4d600e71057f63ef86b89
AmeerLegion <amirharuna.98@gmail.com>
2024-12-05 19:33:33 +0100

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/Layouts/Header";
+import "./styles/App.css";
 
 function App() {
-
 	return (
-    <>
-    <Header/>
-    </>
+		<>
+			<Header />
+		</>
 	);
 }
 
--- src/components/UI/Logo.jsx
+++ src/components/UI/Logo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "../../styles/components/UI/logo.module.css"
+import styles from "../../styles/components/logo.module.css"
 const Logo = () => {
     return(
         <a className={styles.a} href="/">ReactMeals</a>
