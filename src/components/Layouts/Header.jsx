3aaff9d758f2215b05c85514813c3b5f7c6a5551
AmeerLegion <amirharuna.98@gmail.com>
2024-12-07 15:30:22 +0100

    Legion
    More styling added and other work done

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,10 +1,19 @@
 import React from "react";
-import Logo from "../UI/logo"
-
+import styles from "../../styles/layouts/Header.module.css"
+import MealsImage from "../../assets/meals.jpg";
+import Logo from "../UI/logo";
 const Header = () => {
-    return (
-        <Logo></Logo>    
-    )
-}
+	return (
+		<>
+			<header className={styles.header}>
+                <Logo></Logo>
+                <button>Cart</button>
+            </header>
+            <div className={styles['meal-image']}>
+                <img src={MealsImage} alt="A table full of delicious meals" />
+            </div>
+		</>
+	);
+};
 
-export default Header
+export default Header;
