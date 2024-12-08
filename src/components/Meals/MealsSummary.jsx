9992312a51cf96c6e9eb4db525a9e94d4d3de653
AmeerLegion <amirharuna.98@gmail.com>
2024-12-08 15:50:53 +0100

    Legion
    Styling meals summary again

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -4,7 +4,7 @@ import styles from "../../styles/sections/MealsSummary.module.css";
 const MealsSummary = () => {
     return (
         <section className={styles.summary}>
-            <h2>Delicious Food,Delivered to You</h2>
+            <h2>Delicious Food, Delivered to You</h2>
             <p>Chose your favorite meal from our broad selection of available meals and enjoy a delicious lunch or dinner at home.</p>
             <p>All our meals are cooked with high-quality ingredients, just-in-time and of course by experienced chef!</p>
         </section>
