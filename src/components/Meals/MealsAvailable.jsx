09228adf306139a75fb5ee3048e00c99f058bcd4
AmeerLegion <amirharuna.98@gmail.com>
2024-12-10 15:57:22 +0100

    Legion
    Worked on meals section styling and added mixins

--- src/components/Meals/MealsAvailable.jsx
+++ src/components/Meals/MealsAvailable.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import DUMMY_MEALS from "./DUMMY_MEALS"
+import MealItem from "./MealItem";
+import Card from "../UI/Card";
 
 const MealsAvailable = () => {
     return (
-        <section>
-            <ul>
-                {DUMMY_MEALS.map(meal => <li>{meal.name}</li>)}
-        </ul>
-        </section>
-    )
+			<section>
+				<ul>
+					<Card>
+						{DUMMY_MEALS.map((meal) => (
+							<MealItem
+								key={meal.key}
+								name={meal.name}
+								description={meal.description}
+								price={meal.price}
+							></MealItem>
+						))}
+					</Card>
+				</ul>
+			</section>
+		);
 }
 
 export default MealsAvailable;
